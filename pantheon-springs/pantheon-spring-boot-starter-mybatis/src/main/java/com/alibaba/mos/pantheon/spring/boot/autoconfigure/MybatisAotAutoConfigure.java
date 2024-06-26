444c4a9ff92030dcfae7e87a5e9862291d1b85e2
屏崖 <fengqiang.feng@alibaba-inc.com>
2024-06-26 18:53:31 +0800

    feat(mybatis): 避免自动注册的代理类警告

--- pantheon-springs/pantheon-spring-boot-starter-mybatis/src/main/java/com/alibaba/mos/pantheon/spring/boot/autoconfigure/MybatisAotAutoConfigure.java
+++ pantheon-springs/pantheon-spring-boot-starter-mybatis/src/main/java/com/alibaba/mos/pantheon/spring/boot/autoconfigure/MybatisAotAutoConfigure.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 public class MybatisAotAutoConfigure {
 
     @Bean
-    public MybatisMapperFactoryBeanPostProcessor mapperFactoryBeanPostProcessor() {
+    static MybatisMapperFactoryBeanPostProcessor mapperFactoryBeanPostProcessor() {
         return new MybatisMapperFactoryBeanPostProcessor();
     }
 
