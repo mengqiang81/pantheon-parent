dd5a0e00ad097ada3fc7d12d6873b6414314c8a0
屏崖 <fengqiang.feng@alibaba-inc.com>
2024-06-28 18:35:36 +0800

    style(all): 代码格式化

---
+++ pantheon-extends/pantheon-extend-rpc/src/main/java/com/alibaba/mos/pantheon/extend/rpc/exception/DuplicateServiceException.java
@@ -0,0 +1,8 @@
+package com.alibaba.mos.pantheon.extend.rpc.exception;
+
+public class DuplicateServiceException extends RpcException {
+
+    public DuplicateServiceException(String message) {
+        super(message);
+    }
+}
